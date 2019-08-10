21ef9143e5b757e2f8063e2dc24cb1935adedc03
wade <418953115@qq.com>
2019-08-10 18:34:23 +0800

    ok

--- homepage/models.py
+++ homepage/models.py
@@ -5,5 +5,13 @@ from django.db import models
 
 
 class Homepage(models.Model):
-    description = models.CharField(max_length=150)
+    description = models.CharField(default='作品简介',max_length=150)
+    image = models.ImageField(default='default.png',upload_to='images/')
+
+    title = models.CharField(default='作品标题',max_length=50)
+
+
+    def __str__(self):
+        return self.title
+
 
